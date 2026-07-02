649b60753eb4867f5b297c1c0c6c702bec70a235
LozeJoe <lozejoe@users.noreply.github.com>
2026-07-02 13:31:57 +0800

    Remove sensitive files: deploy scripts, logs, backups

--- start-dev.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\Loze\projects\loze-vue-homepage
-npm run dev -- --host 0.0.0.0 --port 5174
--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\Users\Loze\projects\loze-vue-homepage
-echo Starting loze-vue-homepage on port 5174...
-npm run dev
-pause
